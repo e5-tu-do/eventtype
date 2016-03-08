615cc7c1cc169dfe5c3010d8c8aa90fa4b30d59e
schubert.konstantin <schubert.konstantin@gmail.com>
2016-03-08 13:37:47 +0100

    some functonal and stylistic updates

--- eventtype/resolver/views.py
+++ eventtype/resolver/views.py
@@ -13,36 +13,43 @@ def about(request):
 
 
 def resolve(request):
-    event_number = request.GET["event_number_input"]
-    g = int(event_number[0])
-    s = int(event_number[1])
-    d = int(event_number[2])
-    c = int(event_number[3])
-    t = int(event_number[4])
-    n = int(event_number[5])
-    x = int(event_number[6])
-    u = int(event_number[7])
 
     information = {}
 
-    information["g"] = get_g_info(g)
-    information["s"] = get_s_info(s_flag=s, g_flag=g)
-    information["d"] = get_d_info(d_flag=d, g_flag=g, c_flag=c)
-    information["c"] = get_c_info(c_flag=c, g_flag=g)
-    information["t"] = get_t_info(t_flag=t, g_flag=g, c_flag=c)
-    information["n"] = get_n_info(n_flag=n, g_flag=g, c_flag=c, s_flag=s)
-    information["x"] = get_x_info(x_flag=x, g_flag=g, s_flag=s)
-    information["u"] = get_u_info()
+    if "event_number_input" in request.GET:
+        information["has_event_number"] = True
+        event_number = request.GET["event_number_input"]
+        
+        information["event_number"] = event_number
 
-    information["g"]["flag"] = g
-    information["s"]["flag"] = s
-    information["d"]["flag"] = d
-    information["c"]["flag"] = c
-    information["t"]["flag"] = t
-    information["n"]["flag"] = n
-    information["x"]["flag"] = x
-    information["u"]["flag"] = u
+        g = int(event_number[0])
+        s = int(event_number[1])
+        d = int(event_number[2])
+        c = int(event_number[3])
+        t = int(event_number[4])
+        n = int(event_number[5])
+        x = int(event_number[6])
+        u = int(event_number[7])
 
+        information["g"] = get_g_info(g)
+        information["s"] = get_s_info(s_flag=s, g_flag=g)
+        information["d"] = get_d_info(d_flag=d, g_flag=g, c_flag=c)
+        information["c"] = get_c_info(c_flag=c, g_flag=g)
+        information["t"] = get_t_info(t_flag=t, g_flag=g, c_flag=c)
+        information["n"] = get_n_info(n_flag=n, g_flag=g, c_flag=c, s_flag=s)
+        information["x"] = get_x_info(x_flag=x, g_flag=g, s_flag=s)
+        information["u"] = get_u_info()
+
+        information["g"]["flag"] = g
+        information["s"]["flag"] = s
+        information["d"]["flag"] = d
+        information["c"]["flag"] = c
+        information["t"]["flag"] = t
+        information["n"]["flag"] = n
+        information["x"]["flag"] = x
+        information["u"]["flag"] = u
+    else:
+        information["has_event_number"] = False
 
     template = loader.get_template('resolver/resolve.html')
     context = RequestContext(request, information)
