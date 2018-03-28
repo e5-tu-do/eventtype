332d2490b4a7ba1a1243d7ab951579d5ea5049c7
Tobias Tekampe <ttekampe@cern.ch>
2018-03-28 16:52:17 +0200

    removing hard coded path

--- eventtype/eventtype/wsgi.py
+++ eventtype/eventtype/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os, sys
 
 from django.core.wsgi import get_wsgi_application
-sys.path.append('/home/kon/eventtype/eventtype/')
 os.environ["DJANGO_SETTINGS_MODULE"] = "eventtype.settings"
 
 application = get_wsgi_application()
