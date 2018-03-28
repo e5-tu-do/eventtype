cc9d0cabdba53cd8c5fc65430428d6128d711eac
Tobias Tekampe <ttekampe@cern.ch>
2018-03-28 17:16:39 +0200

    fixing typo

--- start.sh
+++ start.sh
@@ -3,14 +3,11 @@
 export PYTHONPATH=${PYTHONPATH}:/eventtype/eventtype/
 
 git clone https://github.com/e5-tu-do/eventtype.git
+# cd eventtype
+# git checkout dev
 
 python eventtype/eventtype/manage.py migrate 
 # python eventtype/eventtype/manage.py runserver
-
-echo $(ls -l)
-RUN cd eventtype
-echo $(ls -l)
-
 
 echo Starting Gunicorn.
 exec gunicorn eventtype.wsgi:application \
