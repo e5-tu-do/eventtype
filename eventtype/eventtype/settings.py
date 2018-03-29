ddc86081edca35f642e8c61212c91d3df8401dd0
Tobias Tekampe <ttekampe@cern.ch>
2018-03-29 12:36:57 +0200

    fix static

--- eventtype/eventtype/settings.py
+++ eventtype/eventtype/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # STATIC_URL = 'http://static.boosted.science/eventtype/'
-STATIC_URL = 'http://cloud.e5.physik.tu-dortmund.de/eventtype/'
+STATIC_URL = 'eventtype/'
 
 # USE_X_FORWARDED_HOST = True
 # FORCE_SCRIPT_NAME = '/eventtype'
