0586df1b4e7d82bfd0aeda0307ccf2fdd498fa37
Tobias Tekampe <ttekampe@cern.ch>
2018-03-29 15:33:20 +0200

    next

--- eventtype/eventtype/settings.py
+++ eventtype/eventtype/settings.py
@@ -64,7 +64,7 @@ def baseurl(request):
     else:
         scheme = 'http://'
 
-    return {'BASE_URL': scheme + request.get_host() + request.path[:-1]}
+    return {'BASE_URL': scheme + "eventtype"}
 
 
 TEMPLATES = [
